85b3a602ec07bbe0c47c65920668f57ebe8aa145
Victor Biga <victor.biga@gmail.com>
2021-10-05 22:08:46 +0100

    fixed json post values for numbers

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type Order struct {
 	Id          bson.ObjectId `json":"id" bson:" _id"`
 	Exchange    string        `json:"exchange" bson: "exchange"`
 	Ticker      string        `json:"ticker" bson: "ticker"`
-	Price       float32       `json:"price" bson: "price"`
-	Amount      float32       `json:"amount" bson: "amount"`
+	Price       string        `json:"price" bson: "price"`
+	Amount      string        `json:"amount" bson: "amount"`
 	OrderAction string        `json:"orderaction" bson: "orderaction"`
 }
